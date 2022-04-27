42c4c9c91a4e37237a14ad7f9fa73e50f6a92a0d
Phillip Rak <rak.phillip@gmail.com>
2022-04-27 16:48:14 -0700

    Update cli flags
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- main.go
+++ main.go
@@ -9,37 +9,60 @@ import (
 )
 
 func main() {
+	var dropletName string
+	var accessToken string
+	var sshFingerprint string
+	var url string
+	var branch string
+	var rancherVersion string
+
 	app := &cli.App{
 		Name:  "Rancher Digital Ocean Provisioner",
 		Usage: "this is a nice usage instruction",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
-				Name:  "droplet-name",
-				Usage: "Name for your Droplet",
+				Name:        "droplet-name",
+				Usage:       "Name for your Droplet",
+				Required:    true,
+				Destination: &dropletName,
 			},
 			&cli.StringFlag{
-				Name:  "url",
-				Usage: "Github url for project",
+				Name:        "access-token",
+				Usage:       "Digital Ocean personal access token",
+				Required:    true,
+				Destination: &accessToken,
 			},
 			&cli.StringFlag{
-				Name:  "branch",
-				Usage: "Git branch provisioning target",
+				Name:        "ssh-fingerprint",
+				Usage:       "Fingerprint for SSH Public Key",
+				Required:    true,
+				Destination: &sshFingerprint,
 			},
 			&cli.StringFlag{
-				Name:  "rancher-version",
-				Usage: "Target version of Rancher",
+				Name:        "url",
+				Usage:       "Github url for project",
+				Required:    true,
+				Destination: &url,
 			},
 			&cli.StringFlag{
-				Name:  "access-token",
-				Usage: "Digital Ocean personal access token",
+				Name:        "branch",
+				Usage:       "Git branch provisioning target",
+				DefaultText: "master",
+				Destination: &branch,
 			},
 			&cli.StringFlag{
-				Name:  "ssh-fingerprint",
-				Usage: "Fingerprint for SSH Public Key",
+				Name:        "rancher-version",
+				Usage:       "Target version of Rancher",
+				DefaultText: "v2.6-head",
+				Destination: &rancherVersion,
 			},
 		},
 		Action: func(c *cli.Context) error {
-			fmt.Println("Some Action")
+			fmt.Println("Provisioning Digital Ocean Droplet...")
+			digitalOceanId, _ := CreateDroplet(dropletName, accessToken, sshFingerprint, url, branch, rancherVersion)
+
+			fmt.Println("Your droplet as been created")
+			fmt.Println("DigitalOcean ID: ", digitalOceanId)
 			return nil
 		},
 	}
@@ -50,9 +73,4 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Provisioning Digital Ocean Droplet...")
-	digitalOceanId, _ := CreateDroplet(Prompt())
-
-	fmt.Println("Your droplet as been created")
-	fmt.Println("DigitalOcean ID: ", digitalOceanId)
 }
