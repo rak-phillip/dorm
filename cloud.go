ce59b69c05f8fe71c03dba4eeeccebb906cef549
Phillip Rak <rak.phillip@gmail.com>
2022-06-27 10:40:33 -0700

    Add CATTLE_BOOTSTRAP_PASSWORD to docker command
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- cloud.go
+++ cloud.go
@@ -18,6 +18,7 @@ func UpdateConfig(fileString string, config *Config) {
 	f = strings.Replace(f, "<REPO_BRANCH>", config.branch, -1)
 	f = strings.Replace(f, "<REPO_URL>", config.url, 1)
 	f = strings.Replace(f, "<RANCHER_VERSION>", config.rancherVersion, 1)
+	f = strings.Replace(f, "<RANCHER_BOOTSTRAP_PASSWORD>", config.bootstrapPassword, 1)
 
 	data := []byte(f)
 	_ = ioutil.WriteFile("./cloud-config", data, 0o600)
